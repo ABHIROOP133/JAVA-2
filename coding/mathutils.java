a39b6bff12ff52ded924209279b840c0056af4f4
ABHIROOP133 <abhiroopchatterjee00@gmail.com>
2026-09-17 10:04:12 +0530

    Delete coding directory

--- coding/mathutils.java
+++
@@ -1,7 +0,0 @@
-
-public class MathUtils {
-
-    public int add(int a, int b) {
-        return a + b;
-    }
-}
--- coding/mathutils/mathfunc.java
+++
@@ -1,7 +0,0 @@
-package mathutils;
-
-public class mathfunc {
-     public int add(int a, int b) {
-        return a + b;
-}
-}
